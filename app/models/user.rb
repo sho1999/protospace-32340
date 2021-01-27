31101fce1c6339f965f9719f57a53ed9646b1717
sho1999 <management14sho@gmail.com>
2021-01-27 21:58:09 +0900

    修正2

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :password, confirmation: true
+  
   validates :name, presence: true
   validates :profile, presence: true
   validates :occupation, presence: true
